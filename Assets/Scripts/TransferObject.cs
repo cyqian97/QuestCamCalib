109c3720cfbca3d57d1292b555a01e0dd107cd90
cyqian97 <cyqian97@hotmail.com>
2024-03-24 16:01:08 -0500

    add arbody package, save setting to json

--- Assets/Scripts/TransferObject.cs
+++ Assets/Scripts/TransferObject.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,15 +10,21 @@ public class TransferObject : MonoBehaviour
 {
     public GameObject model;
     public GameObject detection;
-    public float position_scale;
-    public float rotation_scale;
-    public Vector3 position_offset;
-    public Quaternion direction_offset;
-    public Quaternion rotation_offset;
+    public class CalibData
+    {
+        public float z_scale;
+        // public float rotation_scale;
+        public Vector3 position_offset;
+        public Quaternion direction_offset;
+        public Quaternion rotation_offset;
+    }
+
+    public CalibData cdata;
     // Start is called before the first frame update
     void Start()
     {
-        // direction_offset = Quaternion.identity;
+        cdata = LoadCalibData("CameraCalib.json");
+        // cdata.direction_offset = Quaternion.identity;
 
         // model.SetActive(true);
     }
@@ -27,63 +34,63 @@ public class TransferObject : MonoBehaviour
     {
         if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
         {
-            position_offset.z -= 0.002f;
+            cdata.position_offset.z -= 0.002f;
         }
         if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
         {
-            position_offset.z += 0.002f;
+            cdata.position_offset.z += 0.002f;
         }
         if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp))
         {
-            position_offset.y += 0.002f;
+            cdata.position_offset.y += 0.002f;
         }
         if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown))
         {
-            position_offset.y -= 0.002f;
+            cdata.position_offset.y -= 0.002f;
         }
         if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight))
         {
-            position_offset.x += 0.002f;
+            cdata.position_offset.x += 0.002f;
         }
         if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))
         {
-            position_offset.x -= 0.002f;
+            cdata.position_offset.x -= 0.002f;
         }
 
 
         if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
         {
-            rotation_offset *= Quaternion.AngleAxis(-0.2f, Vector3.forward);
+            cdata.rotation_offset *= Quaternion.AngleAxis(-0.2f, Vector3.forward);
         }
         if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
         {
-            rotation_offset *= Quaternion.AngleAxis(0.2f, Vector3.forward);
+            cdata.rotation_offset *= Quaternion.AngleAxis(0.2f, Vector3.forward);
         }
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp))
         {
-            rotation_offset *= Quaternion.AngleAxis(-0.2f, Vector3.right);
+            cdata.rotation_offset *= Quaternion.AngleAxis(-0.2f, Vector3.right);
         }
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickDown))
         {
-            rotation_offset *= Quaternion.AngleAxis(0.2f, Vector3.right);
+            cdata.rotation_offset *= Quaternion.AngleAxis(0.2f, Vector3.right);
         }
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight))
         {
-            rotation_offset *= Quaternion.AngleAxis(-0.2f, Vector3.up);
+            cdata.rotation_offset *= Quaternion.AngleAxis(-0.2f, Vector3.up);
         }
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft))
         {
-            rotation_offset *= Quaternion.AngleAxis(0.2f, Vector3.up);
+            cdata.rotation_offset *= Quaternion.AngleAxis(0.2f, Vector3.up);
         }
 
 
         if (OVRInput.Get(OVRInput.Button.One))
         {
-            position_scale *= 1.01f;
+            cdata.z_scale *= 1.01f;
         }
         if (OVRInput.Get(OVRInput.Button.Two))
         {
-            position_scale /= 1.01f;
+            cdata.z_scale /= 1.01f;
         }
 
 
@@ -100,28 +107,49 @@ public class TransferObject : MonoBehaviour
         {
             model.SetActive(true);
 
-            Debug.Log(string.Format("position_scale: {0}", position_scale));
-            Debug.Log(string.Format("pos_offset: {0}, {1}, {2}", position_offset.x, position_offset.y, position_offset.z));
-            Debug.Log(string.Format("dir_offset: {0}, {1}, {2}, {3}", direction_offset.x, direction_offset.y, direction_offset.z, direction_offset.w));
-            Debug.Log(string.Format("rot_offset: {0}, {1}, {2}, {3}", rotation_offset.x, rotation_offset.y, rotation_offset.z, rotation_offset.w));
+            Debug.Log(string.Format("cdata.z_scale: {0}", cdata.z_scale));
+            Debug.Log(string.Format("pos_offset: {0}, {1}, {2}", cdata.position_offset.x, cdata.position_offset.y, cdata.position_offset.z));
+            Debug.Log(string.Format("dir_offset: {0}, {1}, {2}, {3}", cdata.direction_offset.x, cdata.direction_offset.y, cdata.direction_offset.z, cdata.direction_offset.w));
+            Debug.Log(string.Format("rot_offset: {0}, {1}, {2}, {3}", cdata.rotation_offset.x, cdata.rotation_offset.y, cdata.rotation_offset.z, cdata.rotation_offset.w));
             Debug.Log(string.Format("Detect pos: {0}, {1}, {2}", detection.transform.position.x, detection.transform.position.y, detection.transform.position.z));
             // Debug.Log(string.Format("Detect rot: {0}, {1}, {2}, {3}", detection.transform.rotation.x, detection.transform.rotation.y, detection.transform.rotation.z, detection.transform.rotation.w));
 
             var _pos = detection.transform.position;
-            _pos.z *= position_scale;
-            model.transform.localPosition = (direction_offset * _pos) + position_offset;
+            _pos.z *= cdata.z_scale;
+            model.transform.localPosition = (cdata.direction_offset * _pos) + cdata.position_offset;
 
 
             // float angle = 0.0f;
             // Vector3 axis = Vector3.zero;
             // detection.transform.rotation.ToAngleAxis(out angle, out axis);
             // angle *= rotation_scale;
-            // model.transform.localRotation = Quaternion.AngleAxis(angle, axis) * direction_offset;
-            model.transform.localRotation = direction_offset*rotation_offset*detection.transform.rotation;
+            // model.transform.localRotation = Quaternion.AngleAxis(angle, axis) * cdata.direction_offset;
+            model.transform.localRotation = cdata.direction_offset * detection.transform.rotation * cdata.rotation_offset;
         }
         else
         {
             model.SetActive(false);
         }
+    }
+
+    void OnApplicationQuit()
+    {
+        SaveCalibData(cdata,"CameraCalib.json");
+    }
+
+    public static CalibData LoadCalibData(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            return JsonUtility.FromJson<CalibData>(jsonData);
+        }
+        return new CalibData(); 
+    }
+
+    public static void SaveCalibData(CalibData data, string filePath)
+    {
+        string jsonData = JsonUtility.ToJson(data, true);
+        File.WriteAllText(filePath, jsonData);
     }
 }
